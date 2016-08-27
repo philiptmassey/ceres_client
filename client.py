117c3b913eea1f12b6e07feb0df2ca1bea0a612c
philiptmassey <philip.t.massey@gmail.com>
2016-08-26 18:46:58 -0700

    datadump -> data

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import requests
 
 SERVER_URL = "https://protected-everglades-36173.herokuapp.com"
 ADD_URL = "%s/add" % SERVER_URL
-DATA_URL = "%s/datadump" % SERVER_URL
+DATA_URL = "%s/data" % SERVER_URL
 
 def test_post():
     json = {"id": "test_python_id", "data": "test_data"}
@@ -20,7 +20,7 @@ def get_data():
         print result["data"]
 
 def main():
-    test_post()
+    #test_post()
     get_data()
 
 if __name__ == "__main__":
